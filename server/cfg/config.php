1331ac569421c8a4a940cb7ea7cac6734bfb5069
dgdagadin87 <dgdagadin87@mail.ru>
2016-10-23 16:54:59 +0300

    Added config file

---
+++ server/cfg/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define ('CONNECTION_HOST'    , 'localhost'); //Сервер БД
+define ('CONNECTION_USER'    , 'root'); //Пользователь БД
+define ('CONNECTION_PASSWORD', 'root'); //Пароль БД
+define ('CONNECTION_DB'      , '4stor'); //Название БД
