fc8d7bc814531f062b3013dfccb0dec0059d5dcd
dgdagadin87 <dgdagadin87@mail.ru>
2016-10-23 16:54:39 +0300

    Added sql-scheme

---
+++ server/sql/db.sql
@@ -0,0 +1,52 @@
+CREATE TABLE categories 
+(
+    catId INT AUTO_INCREMENT NOT NULL , 
+    catName VARCHAR(200) NOT NULL , 
+    catHref VARCHAR(200) NOT NULL , 
+  
+    PRIMARY KEY ( catId ) , 
+    UNIQUE ( catName ) , 
+    UNIQUE ( catHref )
+) 
+DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ENGINE = MYISAM;
+
+CREATE TABLE authors 
+(
+    authorId INT AUTO_INCREMENT NOT NULL , 
+    authorName VARCHAR(200) NOT NULL , 
+    authorHref VARCHAR(200) NOT NULL , 
+  
+    PRIMARY KEY ( authorId ) , 
+    UNIQUE ( authorName ) , 
+    UNIQUE ( authorHref )
+) 
+DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ENGINE = MYISAM;
+
+CREATE TABLE stories 
+(
+    storId BIGINT AUTO_INCREMENT NOT NULL , 
+    storName TEXT NOT NULL , 
+    storHref TEXT NOT NULL , 
+    storRate INT NOT NULL , 
+    storDesc TEXT NOT NULL , 
+    storContent LONGTEXT ,
+    storAuthorId INT NOT NULL , 
+  
+    PRIMARY KEY ( storId )
+) 
+DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ENGINE = MYISAM;
+
+CREATE TABLE cats2stories 
+(
+    cat2storId BIGINT AUTO_INCREMENT NOT NULL , 
+    storId BIGINT NOT NULL , 
+    catId INT NOT NULL ,
+  
+    PRIMARY KEY ( cat2storId ) , 
+    UNIQUE ( storId, catId )
+) 
+DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci
+ENGINE = MYISAM;
