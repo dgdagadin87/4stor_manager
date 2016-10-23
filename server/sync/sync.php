716d8b48eb287323e95432ade2f8dab350ea35d9
dgdagadin87 <dgdagadin87@mail.ru>
2016-10-23 21:18:58 +0300

    Modified sync functionality

--- server/sync/sync.php
+++ server/sync/sync.php
@@ -6,6 +6,8 @@ require ('../inc/db.inc.php');
 require ('sync.class.php');
 require ('sync.model.php');
 
+header('Content-Type: text/html; charset=utf-8');
+
 $sync = new serverSync(array(
     'model'   => new serverSyncModel(),
     'href'    => 'http://4stor.ru/histori-for-life/page',
