8ca6a367f4c724755da8ca534139c11e8370f195
Dmitry <dgdagadin87@mail.ru>
2016-10-28 14:05:31 +0300

    Added ajax core functions

--- resources/js/_core/coreUtils.js
+++ resources/js/_core/coreUtils.js
@@ -3,11 +3,13 @@
 define([
     'underscore',
     'backbone', 
-    'jquery'
+    'jquery',
+    'Application'
 ], function (
     _,
     Backbone,
-    $
+    $,
+    Application
 ) {
     return {
 
@@ -19,6 +21,63 @@ define([
             var currentURI = this.getCurrentRoute();
             var laParams = currentURI.split('/') || [];
             return laParams;
+        },
+        
+        isEmpty : function(value){
+            if (value === null || value === '' || value === undefined ){
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
+        
+        emptyFunction: function () {
+            return;
+        },
+        
+        applyParams: function(object, config) {
+            var property;
+            if (object) {
+                for (property in config) {
+                    object[property] = config[property];
+                }
+            }
+            return object;
+        },
+        
+        axajQuery: function(config, functions){
+            var loConf = config || {};
+            var BeforeSend    = lfIsEmpty(functions.beforeSend)    || !$.isFunction(functions.beforeSend)    ? this.emptyFunction : functions.beforeSend;
+            var AfterSuccess  = lfIsEmpty(functions.afterSuccess)  || !$.isFunction(functions.afterSuccess)  ? this.emptyFunction : functions.afterSuccess;
+            var AfterError    = lfIsEmpty(functions.afterError)    || !$.isFunction(functions.afterError)    ? this.emptyFunction : functions.afterError;
+            var AfterComplete = lfIsEmpty(functions.afterComplete) || !$.isFunction(functions.afterComplete) ? this.emptyFunction : functions.afterComplete;
+                
+            var queryConfig = {
+                    type: 'POST',
+                    async: true,
+                    cache: false,
+                    dataType: 'json',
+                    headers: {
+                        Connection: 'close'
+                    },
+                    beforeSend: function(jqXHR, settings){
+                        BeforeSend(jqXHR, settings);
+                    },
+                    success: function(data, textStatus, jqXHR){
+                        AfterSuccess(data, textStatus, jqXHR);
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        alert('На сервере какая-то ошибка');
+                        AfterError(jqXHR, textStatus, errorThrown);
+                    },
+                    complete: function(jqXHR, textStatus){
+                        AfterComplete(jqXHR, textStatus);
+                    }
+                };
+            var cfg = this.applyParams(loConf, queryConfig);
+            
+            $.ajax(cfg);
         }
 
     };
