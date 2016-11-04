cc4112344cfb4dd95831d9987490d8820154eadc
dgdagadin87 <dgdagadin87@mail.ru>
2016-11-04 21:46:32 +0300

    Index action was improved

--- server/api/actions/index/index.model.php
+++ server/api/actions/index/index.model.php
@@ -30,6 +30,25 @@ class indexModel {
         return $laReturn;
     }
     
+    public function getAuthors ($authorIds) {
+        $SQL = 'SELECT * FROM authors WHERE authorId IN (' . implode(',', $authorIds) . ')';
+        $Query = DB_Query ('mysql', $SQL, $this->connection);
+        if (!$Query) {
+            return 'Ошибка при получении данных авторов';
+        }
+        $authors = array();
+        while($Data = DB_FetchAssoc ('mysql', $Query)) {
+            $authorId = $Data['authorId'];
+            $authorName = $Data['authorName'];
+            $authorHref = $Data['authorHref'];
+            $authors[$authorId] = array(
+                'authorName' => $Data['authorName'],
+                'authorHref' => $Data['authorHref'],
+            );
+        }
+        return ($authors);
+    }
+    
     public function setCatsIdByName () {
         $SQL = 'SELECT catId, catName FROM categories WHERE catName IN (\'Творческие истории\', \'Природные аномальные явления\', \'Истории из жизни\')';
         $Query = DB_Query ('mysql', $SQL, $this->connection);
@@ -57,15 +76,19 @@ class indexModel {
             . 'UNION '
             . '(SELECT c.*, c2s.catId as cid,s.* FROM cats2stories c2s LEFT JOIN stories s ON c2s.storId = s.storId LEFT JOIN categories c ON c2s.catId = c.catId WHERE c2s.catId IN (' . $this->artHistoriesId . ') ORDER BY s.storDate DESC LIMIT 5) '
             . 'UNION '
-            . '(SELECT c.*, c2s.catId as cid,s.* FROM cats2stories c2s LEFT JOIN stories s ON c2s.storId = s.storId LEFT JOIN categories c ON c2s.catId = c.catId WHERE c2s.catId IN (' . $this->lifeHistoriesId . ') ORDER BY s.storDate DESC LIMIT 5)';
+            . '(SELECT c.*, c2s.catId as cid,s.* FROM cats2stories c2s LEFT JOIN stories s ON c2s.storId = s.storId LEFT JOIN categories c ON c2s.catId = c.catId WHERE c2s.catId IN (' . $this->natHistoriesId . ') ORDER BY s.storDate DESC LIMIT 5)';
         $Query = DB_Query ('mysql', $SQL, $this->connection);
         if (!$Query) {
             return 'Ошибка при получении данных главной страницы';
         }
         $tmpIndex = array();
+        $authorIds = array();
         while($Data = DB_FetchAssoc ('mysql', $Query)) {
             $catId = $Data['cid'];
             $storId = $Data['storId'];
+            if (!array_key_exists($Data['storAuthorId'], $authorIds)) {
+                $authorIds[] = $Data['storAuthorId'];
+            }
             if (!isset($tmpIndex[$catId])) {
                 $tmpIndex[$catId] = array(
                     'categoryId' => $Data['cid'],
@@ -80,8 +103,9 @@ class indexModel {
                     'storName'       => $Data['storName'],
                     'storHref'       => $Data['storHref'],
                     'storShortDesc'  => $Data['storDesc'],
-                    'storAuthorName' => 'Author',
-                    'storAuthorHref' => 'hhh',
+                    'storAuthorId'   => $Data['storAuthorId'],
+                    'storAuthorName' => '',
+                    'storAuthorHref' => '',
                     'storRate'       => $Data['storRate'],
                     'storDate'       => $Data['storDate'],
                     'storWatches'    => $Data['storWatches'],
@@ -90,9 +114,15 @@ class indexModel {
             }
         }
         
+        $authors = $this->getAuthors($authorIds);
+        
         $tmpIndex = array_values($tmpIndex);
         foreach ($tmpIndex as $indexKey=>$indexValue) {
             $tmpIndex[$indexKey]['stors'] = array_values($indexValue['stors']);
+            foreach ($tmpIndex[$indexKey]['stors'] as $k=>$v) {
+                $tmpIndex[$indexKey]['stors'][$k]['storAuthorName'] = $authors[$v['storAuthorId']]['authorName'];
+                $tmpIndex[$indexKey]['stors'][$k]['storAuthorHref'] = $authors[$v['storAuthorId']]['authorHref'];
+            }
         }
         
         return ($tmpIndex);
