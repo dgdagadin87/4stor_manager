17ce7328b85295171136c1db327a914311a2404a
dgdagadin87 <dgdagadin87@mail.ru>
2016-11-11 00:10:35 +0300

    *

--- resources/js/modules/category/categoryController.js
+++ resources/js/modules/category/categoryController.js
@@ -7,8 +7,9 @@ define([
     'coreUtils',
     'Application',
     'settings',
-    '_base/BaseController'
-    //'modules/storelist/views/categoryView'
+    '_base/BaseController',
+    'modules/category/models/categoryStateModel',
+    'modules/category/views/categoryView'
 ], function (
     _,
     Backbone,
@@ -16,8 +17,9 @@ define([
     CoreUtils,
     Application,
     Settings,
-    BaseController
-    //categoryView
+    BaseController,
+    metaModel,
+    categoryView
 ) {
     var categoryController = function(poConfig) {
         
@@ -28,13 +30,14 @@ define([
         
         BaseController.call(this);
         
+        this._meta = new metaModel();
+        
         this._categoryId = null;
         
-        this._isStorlistRendered = false;
-        this._isDataLoaded = false;
+        this._isCategoryRendered = false;
         this._breadCrumbs = [];
 
-        //this._view = new categoryView();
+        this._view = new categoryView();
         
         this._init();
         this._bindEvents();
@@ -43,7 +46,8 @@ define([
     categoryController.prototype = Object.create(BaseController.prototype);
     
     categoryController.prototype._bindEvents = function() {
-        //this._view.on('render', this._onViewRendered.bind(this));
+        this._view.on('render', this._onViewRendered.bind(this));
+        this._meta.on('change');
     };
     
     categoryController.prototype._init = function() {
@@ -58,59 +62,62 @@ define([
         var catId = poParams.categoryId || 0;
         if (catId !== this._categoryId) {
             this._categoryId = catId;
+            this._showCurrentContent()
         }
     };
+    
+    categoryController.prototype._renderComponents = function() {
+        
+    };
 
-//    categoryController.prototype.showCurrentContent = function(poParams) {
-//        var me = this;
-//        var mainLayout = Application.getMainLayout();
-//        var layoutView = mainLayout.getView();
-//        var lfRender = function(){
-//            if (!me.__isStorlistRendered) {
-//                layoutView[me._regionName].show(me.getView(), {forceShow: true});
-//            }
-//            else {
-//                me.renderView();
-//            }
-//        };
-//        
-//        if (!me._isDataLoaded) {
-//            var afterSuccess = function(data) {
-//                var laData = data.data || [];
-//                var indexData = laData.index || [];
-//                var breadCrumbsData = laData.breadcrumbs || [];
-//                var lbSuccess = data.success || false;
-//                var lsMessage = data.message || '';
-//                if (!lbSuccess) {
-//                    Application.trigger('error:modal:show', lsMessage);
-//                }
-//                else {
-//                    me._isDataLoaded = true;
-//                    me._breadCrumbs = breadCrumbsData;
-//                    Application.trigger('breadcrumbs:show', breadCrumbsData);
-//                    me.getView().collection.set(indexData);
-//                    lfRender();
-//                }
-//            };
-//            var afterError = function(data){
-//                var lsMessage = data.message || '';
-//                Application.trigger('error:modal:show', lsMessage);
-//            };
-//            Application.trigger('breadcrumbs:hide');
-//            Application.trigger('spinner:large:show', this._regionName, 'Идет загрузка данных...');
-//            CoreUtils.axajQuery({
-//                url: Settings.url.getIndexData
-//            },
-//            {
-//                afterSuccess: afterSuccess,
-//                afterError: afterError
-//            });
-//        }
-//        else {
-//            Application.trigger('breadcrumbs:show', me._breadCrumbs);
-//            lfRender();
-//        }
-//    };
+    categoryController.prototype._showCurrentContent = function() {
+        var me = this;
+        var mainLayout = Application.getMainLayout();
+        var layoutView = mainLayout.getView();
+        var lfRender = function(){
+            if (!me._isCategoryRendered) {
+                layoutView[me._regionName].show(me.getView(), {forceShow: true});
+            }
+            else {
+                me.renderView();
+            }
+        };
+
+        var afterSuccess = function(data) {
+            var laData = data.data || [];
+            var categoryData = laData.category || [];
+            var breadCrumbsData = laData.breadcrumbs || [];
+            var lbSuccess = data.success || false;
+            var lsMessage = data.message || '';
+            if (!lbSuccess) {
+                Application.trigger('error:modal:show', lsMessage);
+            }
+            else {
+                Application.trigger('breadcrumbs:show', breadCrumbsData);
+                //me.getView().collection.set(indexData);
+                lfRender();
+            }
+        };
+        var afterError = function(data){
+            var lsMessage = data.message || '';
+            Application.trigger('error:modal:show', lsMessage);
+        };
+        Application.trigger('breadcrumbs:hide');
+        Application.trigger('spinner:large:show', this._regionName, 'Идет загрузка данных...');
+        CoreUtils.axajQuery({
+            url: Settings.url.getCategoryData,
+            data: {
+                catId   : me._categoryId,
+                page    : me._meta.get('page'),
+                sortBy  : me._meta.get('sortBy'),
+                sortType: me._meta.get('sortType')
+            }
+        },
+        {
+            afterSuccess: afterSuccess,
+            afterError: afterError
+        });
+    };
 
     categoryController.prototype.renderView = function() {
         this.getView().render();
