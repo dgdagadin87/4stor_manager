174c3d3822a187569bb6c1934592ccdf1dae4249
dgdagadin87 <dgdagadin87@mail.ru>
2016-11-13 19:04:56 +0300

    Abstract base controller was improved

--- resources/js/_base/BaseController.js
+++ resources/js/_base/BaseController.js
@@ -15,6 +15,7 @@ define([
 
         this._view = null;
         this._isAdded = false;
+        this._isGlobalLoading = false;
     };
 
     BaseController.prototype.destructor = function() {
@@ -23,6 +24,20 @@ define([
             delete this._view;
         }
         console.log('Module destructor');
+    };
+
+    BaseController.prototype.__renderContent = function() {
+        Application.trigger('content:regions:hide');
+        Application.trigger('content:region:show', this._regionName);
+        Application.trigger('breadcrumbs:show', this._breadCrumbs);
+        Application.trigger('title:change', this._pageTitle);
+    };
+    
+    BaseController.prototype.__renderSpinner = function() {
+        Application.trigger('breadcrumbs:hide');
+        Application.trigger('content:regions:hide');
+        Application.trigger('spinner:large:show', 'Идет загрузка данных...');
+        Application.trigger('title:change', 'Идет загрузка данных...');
     };
 
     BaseController.prototype.getView = function() {
