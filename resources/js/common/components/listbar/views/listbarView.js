158a2c08d8109c9d3d486aa6f0884ff686ed4a3e
dgdagadin87 <dgdagadin87@mail.ru>
2016-11-16 00:48:27 +0300

    Listbar component was modified

--- resources/js/common/components/listbar/views/listbarView.js
+++ resources/js/common/components/listbar/views/listbarView.js
@@ -32,8 +32,11 @@ define(
                 var sortBy = current.attr('sort-name');
                 var opts = this.options || {};
                 var eventPrefix = opts.eventPrefix || 'category';
+                var curSortType = current.hasClass('sort-desc') || (!current.hasClass('sort-desc') && !current.hasClass('sort-asc')) ? 'DESC' : 'ASC';
+                var sortType = curSortType === 'DESC' ? 'ASC' : 'DESC';
                 Application.trigger(eventPrefix+':sort:change', {
-                    sortBy: sortBy
+                    sortBy: sortBy,
+                    sortType: sortType
                 });
             },
             
