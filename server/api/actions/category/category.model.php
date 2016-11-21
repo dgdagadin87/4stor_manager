612991fdc988ffd0e2c3e5dcfb48775f53c28d41
1 <1@1-pc>
2016-11-21 23:29:25 +0300

    Server abstract model was added

--- server/api/actions/category/category.model.php
+++ server/api/actions/category/category.model.php
@@ -1,11 +1,7 @@
 <?php
 
-class categoryModel {
-    
-    public $connection = null;
-    
-    public $categories = array();
-    
+class categoryModel extends abstractModel {
+
     public $categoryId   = null;
     public $categoryName = null;
     
@@ -20,27 +16,6 @@ class categoryModel {
     public function run () {
         $this->connect();
         return $this->getCategory();
-    }
-    
-    public function getFullStorCatList ($storIds) {
-        $laStors = array();
-        $SQL = 'SELECT catId, storId FROM cats2stories WHERE storId IN (' . implode(',', $storIds) . ')';
-        $Query = DB_Query ('mysql', $SQL, $this->connection);
-        if (!$Query) {
-            return 'Ошибка при получении полного списка категорий рассказов';
-        }
-        while($Data = DB_FetchAssoc ('mysql', $Query)) {
-            $storId = $Data['storId'];
-            $catId  = $Data['catId'];
-            if (!isset($laStors[$storId])) {
-                $laStors[$storId] = array();
-            }
-            $laStors[$storId][] = array(
-                'catId' => $catId,
-                'catName' => $this->categories[$catId]
-            );
-        }
-        return $laStors;
     }
     
     public function getMeta() {
@@ -98,23 +73,6 @@ class categoryModel {
         $this->numOfStors = $numStores;
         
         return (true);
-    }
-    
-    public function getAuthors ($authorIds) {
-        $SQL = 'SELECT * FROM authors WHERE authorId IN (' . implode(',', $authorIds) . ')';
-        $Query = DB_Query ('mysql', $SQL, $this->connection);
-        if (!$Query) {
-            return 'Ошибка при получении данных авторов';
-        }
-        $authors = array();
-        while($Data = DB_FetchAssoc ('mysql', $Query)) {
-            $authorId = $Data['authorId'];
-            $authors[$authorId] = array(
-                'authorName' => $Data['authorName'],
-                'authorHref' => $Data['authorHref'],
-            );
-        }
-        return ($authors);
     }
     
     public function getCategory () {
@@ -206,30 +164,6 @@ class categoryModel {
                 'name' => $this->categoryName
             )
         );
-    }
-    
-    public function getSortByArray() {
-        return array(
-            'storDate',
-            'storRate',
-            'storComments',
-            'storWatches',
-            'storName'
-        );
-    }
-    
-    public function getSortTypeArray() {
-        return array(
-            'ASC',
-            'DESC'
-        );
-    }
-    
-    public function connect () {
-        if (is_null($this->connection)) {
-            $this->connection = DB_Connect('mysql');
-        }
-        DB_SetUTF8($this->connection, 'mysql');
     }
     
 }
