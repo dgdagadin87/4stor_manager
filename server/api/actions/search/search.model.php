453f12028b1347755ace81c06f41f6f12bb197aa
1 <1@1-pc>
2016-11-21 23:20:31 +0300

    Server abstract model was added

--- server/api/actions/search/search.model.php
+++ server/api/actions/search/search.model.php
@@ -1,10 +1,6 @@
 <?php
 
-class searchModel {
-    
-    public $connection = null;
-    
-    public $categories = array();
+class searchModel extends abstractModel {
 
     public $numPages = 1;
     public $curPage  = 1;
@@ -27,27 +23,6 @@ class searchModel {
     public function run () {
         $this->connect();
         return $this->getSearch();
-    }
-    
-    public function getFullStorCatList ($storIds) {
-        $laStors = array();
-        $SQL = 'SELECT catId, storId FROM cats2stories WHERE storId IN (' . implode(',', $storIds) . ')';
-        $Query = DB_Query ('mysql', $SQL, $this->connection);
-        if (!$Query) {
-            return 'Ошибка при получении полного списка категорий рассказов';
-        }
-        while($Data = DB_FetchAssoc ('mysql', $Query)) {
-            $storId = $Data['storId'];
-            $catId  = $Data['catId'];
-            if (!isset($laStors[$storId])) {
-                $laStors[$storId] = array();
-            }
-            $laStors[$storId][] = array(
-                'catId' => $catId,
-                'catName' => $this->categories[$catId]
-            );
-        }
-        return $laStors;
     }
     
     public function getMeta() {
@@ -138,24 +113,7 @@ class searchModel {
         
         return (true);
     }
-    
-    public function getAuthors ($authorIds) {
-        $SQL = 'SELECT * FROM authors WHERE authorId IN (' . implode(',', $authorIds) . ')';
-        $Query = DB_Query ('mysql', $SQL, $this->connection);
-        if (!$Query) {
-            return 'Ошибка при получении данных авторов';
-        }
-        $authors = array();
-        while($Data = DB_FetchAssoc ('mysql', $Query)) {
-            $authorId = $Data['authorId'];
-            $authors[$authorId] = array(
-                'authorName' => $Data['authorName'],
-                'authorHref' => $Data['authorHref'],
-            );
-        }
-        return ($authors);
-    }
-    
+
     public function getSearch () {
         
         $metaResult = $this->getMeta();
@@ -276,30 +234,6 @@ class searchModel {
         $lsSql = ' ' . implode(' AND ', $laConditions) . ' ';
         
         return ($lsSql);
-    }
-    
-    public function getSortByArray() {
-        return array(
-            'storDate',
-            'storRate',
-            'storComments',
-            'storWatches',
-            'storName'
-        );
-    }
-    
-    public function getSortTypeArray() {
-        return array(
-            'ASC',
-            'DESC'
-        );
-    }
-    
-    public function connect () {
-        if (is_null($this->connection)) {
-            $this->connection = DB_Connect('mysql');
-        }
-        DB_SetUTF8($this->connection, 'mysql');
     }
     
 }
