fa0a148c8573e631cb61c4a269b226e2a5d907e4
dgdagadin87 <dgdagadin87@mail.ru>
2016-11-27 21:07:54 +0300

    Statistics api was modified

--- server/api/actions/statistics/statistics.model.php
+++ server/api/actions/statistics/statistics.model.php
@@ -16,7 +16,7 @@ class statisticsModel extends abstractModel {
         $statistics = $this->getAppStat();
         if (!is_array($statistics)) {
             $laReturn['success'] = false;
-            $laReturn['message'] = $index;
+            $laReturn['message'] = $statistics;
             return $laReturn;
         }
         $laReturn['data']['statistics'] = $statistics;
@@ -24,19 +24,28 @@ class statisticsModel extends abstractModel {
     }
 
     public function getAppStat() {
-        return array(
-            'numOfCategories'     => 19,
-            'numOfStories'        => 20000,
-            'numOfAuthors'        => 3000,
-            'mostPopularCatId'    => 1,
-            'mostPopularCatName'  => 'Истории из жизни',
-            'lessPopularCatId'    => 9,
-            'lessPopularCatName'  => 'Страшные арты',
-            'mostPopularStorId'   => 25772,
-            'mostPopularStorName' => 'НЛО, доказательства существования',
-            'lessPopularStorId'   => 854,
-            'lessPopularStorName' => 'Фотографии НЛО',
-        );
+        
+        // данные о категориях
+        $SQL = 'SELECT c.*, MAX(s.storRate) as maxRate, AVG(s.storRate) as avgRate, MIN(s.storRate) AS minRate, COUNT(DISTINCT s.storId) AS numOfStors FROM cats2stories c2s LEFT JOIN stories s ON c2s.storId = s.storId LEFT JOIN categories c ON c2s.catId = c.catId GROUP BY c2s.catId';
+        $Query = DB_Query ('mysql', $SQL, $this->connection);
+        if (!$Query) {
+            return 'Ошибка при получении данных о категориях';
+        }
+
+        $statistics = array();
+        while($Data = DB_FetchAssoc ('mysql', $Query)) {
+            $statistics[] = array(
+                'catId'      => $Data['catId'],
+                'catName'    => $Data['catName'],
+                'catHref'    => $Data['catHref'],
+                'maxRate'    => number_format ($Data['maxRate'], 0, '.', ' '),
+                'avgRate'    => number_format ($Data['avgRate'], 2, '.', ' '),
+                'minRate'    => number_format ($Data['minRate'], 0, '.', ' '),
+                'numOfStors' => number_format ($Data['numOfStors'], 0, '.', ' ')
+            );
+        }
+        
+        return ($statistics);
     }
     
 }
