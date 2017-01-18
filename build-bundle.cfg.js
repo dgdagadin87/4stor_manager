9494222b70f4370034492df2889a2b271d25bfa4
dgdagadin87 <dgdagadin87@mail.ru>
2017-01-19 00:08:45 +0300

    Config file for bundling project by r.js was added

---
+++ build-bundle.cfg.js
@@ -0,0 +1,8 @@
+({
+    baseUrl: './resources/js',
+    name: "./start",
+    mainConfigFile: 'resources/js/start.js',
+    include: ["library/require/require"],
+    out: './builds/bundle.js',
+    optimize: 'none'
+});
