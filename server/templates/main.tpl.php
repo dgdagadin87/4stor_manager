bdf32d894083fe08bd4c10289b4b272461994036
dgdagadin87 <dgdagadin87@mail.ru>
2017-02-19 00:39:19 +0300

    version bundle

--- server/templates/main.tpl.php
+++ server/templates/main.tpl.php
@@ -5,7 +5,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta content="ie=edge" http-equiv="x-ua-compatible">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script data-main="resources/js/start.js" src="resources/js/library/require/require.js"></script>
+        <script src="resources/js/bundle.js"></script>
         <link href="http://allfont.ru/allfont.css?fonts=pt-sans" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="resources/css/jquery-ui.css"/>
         <link rel="stylesheet" href="resources/css/chartist.min.css"/>
